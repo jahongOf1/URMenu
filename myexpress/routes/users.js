ad6d8f99b38bc227de6ea149668abc2198816059
xinyi <xinyi2017mm@gmail.com>
2019-04-08 15:26:29 -0700

    add the insert folder containing code insert data into db, add the retrieve folder containing code get data from db

--- myexpress/routes/users.js
+++ myexpress/routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+/* GET user's list'.  ?? how to*/
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(`show all users list?`);
 });
+
+/* GET user's home info'. */
+router.get('/:home', function(req, res, next) {
+  res.send(`user wants to get name ${req.params.home}`);
+});
+
+/* GET user's like info'. */
+router.get('/:like', function(req, res, next) {
+  res.send(`user wants to get name ${req.params.home}`);
+});
+
+/* GET user's coupon info'. */
+router.get('/:coupon', function(req, res, next) {
+  res.send(`user wants to get name ${req.params.home}`);
+});
+
+/* GET user's home info'. */
+router.get('/:review', function(req, res, next) {
+  res.send(`user wants to get name ${req.params.home}`);
+});
+
+/* GET user's review info'. */
+router.get('/:home', function(req, res, next) {
+  res.send(`user wants to get name ${req.params.home}`);
+});
+
+/* Post user's review to specific restaurant. */
+
 
 module.exports = router;
