88c22ea8724d7e3ee4c4e7597785cfd753b3c4b9
xinyi <xinyi2017mm@gmail.com>
2019-04-09 20:23:03 -0700

    add the searching box. create a css folder

---
+++ client/src/components/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import '../css/Search.css'
+
+class Search extends Component {
+ state = {
+   query: '',
+ }
+
+ handleInputChange = () => {
+   this.setState({
+     query: this.search.value
+   })
+ }
+
+ render() {
+   return (
+     <form >
+       <input
+         placeholder="Search for..."
+         ref={input => this.search = input}
+         onChange={this.handleInputChange}
+       />
+       {/* <p>{this.state.query}</p> */}
+     </form>
+   )
+ }
+}
+
+export default Search
