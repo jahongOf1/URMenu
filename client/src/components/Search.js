909f6fe56facf9f2f4306c29fdc8ed851e324d07
xinyi <xinyi2017mm@gmail.com>
2019-04-10 17:10:48 -0700

    add

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import ResultList from './ResultList.js'
+
+// const { API_KEY } = process.env
+const API_URL = 'https://restcountries.eu/rest/v2/name'
 
 class Search extends Component {
- state = {
-   query: '',
- }
+  state = {
+    query: '',
+    results: []
+  }
 
- handleInputChange = () => {
-   this.setState({
-     query: this.search.value
-   })
- }
+  getInfo = () => {
+    axios.get(`${API_URL}/${this.state.query}`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.data
+        })
+      })
+  }
 
- render() {
-   return (
-     <form >
-       <input
-         placeholder="Search for..."
-         ref={input => this.search = input}
-         onChange={this.handleInputChange}
-       />
-       {/* <p>{this.state.query}</p> */}
-     </form>
-   )
- }
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      } else if (!this.state.query) {
+      }
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <ResultList results={this.state.results} />
+      </form>
+    )
+  }
 }
 
 export default Search
